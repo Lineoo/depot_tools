76f93b25bb0492708f23878e485e6dac4b055773
Alan Zhao <crazy_alan@163.com>
2025-12-01 14:11:38 +0800

    Support FocusMgr

--- widget/src/ui_control/util/focus_mgr.rs
+++ widget/src/ui_control/util/focus_mgr.rs
@@ -1,20 +1,22 @@
 use indexmap::IndexSet;
 
-use crate::ui_control::control::{self, Control, WeakHandle};
+use crate::ui_control::control::{Control, WeakHandle};
 
 pub struct FocusMgr {
     chain: IndexSet<WeakHandle<dyn Control>>,
+    current_idx: usize,
 }
 
 impl FocusMgr {
     pub(crate) fn new() -> Self {
         Self {
             chain: IndexSet::new(),
+            current_idx: 0,
         }
     }
 
     pub fn set_order(&mut self, prev: WeakHandle<dyn Control>, control: WeakHandle<dyn Control>) {
-        if (!self.chain.contains(&prev)) {
+        if !self.chain.contains(&prev) {
             self.chain.insert(prev.clone());
         }
         self.insert_after(prev, control);
@@ -32,10 +34,13 @@ impl FocusMgr {
         if !self.chain.contains(&prev) {
             return Err(FocusMgrInsertFailure::PrevNotExist);
         }
-        let (_, r) = self
+        let (idx, r) = self
             .chain
             .insert_before(self.chain.get_index_of(&prev).unwrap() + 1, control);
         Ok(if r {
+            if idx <= self.current_idx {
+                self.current_idx += 1;
+            }
             FocusMgrInsertState::InsertSuccess
         } else {
             FocusMgrInsertState::MoveOrder
@@ -43,6 +48,11 @@ impl FocusMgr {
     }
 
     pub fn remove(&mut self, control: WeakHandle<dyn Control>) {
+        if let Some(idx) = self.chain.get_index_of(&control)
+            && idx < self.current_idx
+        {
+            self.current_idx -= 1;
+        }
         self.chain.shift_remove(&control);
     }
 
@@ -53,7 +63,7 @@ impl FocusMgr {
     pub fn query_prev(
         &self,
         control: WeakHandle<dyn Control>,
-    ) -> Result<(WeakHandle<dyn Control>), FocusMgrQueryFailure> {
+    ) -> Result<WeakHandle<dyn Control>, FocusMgrQueryFailure> {
         if !self.chain.contains(&control) {
             return Err(FocusMgrQueryFailure::NotExist);
         }
@@ -69,7 +79,7 @@ impl FocusMgr {
     pub fn query_next(
         &self,
         control: WeakHandle<dyn Control>,
-    ) -> Result<(WeakHandle<dyn Control>), FocusMgrQueryFailure> {
+    ) -> Result<WeakHandle<dyn Control>, FocusMgrQueryFailure> {
         if !self.chain.contains(&control) {
             return Err(FocusMgrQueryFailure::NotExist);
         }
@@ -80,6 +90,25 @@ impl FocusMgr {
             idx + 1
         }]
         .clone());
+    }
+
+    pub fn prev(&mut self) {
+        if self.current_idx == 0 {
+            self.current_idx = self.chain.len() - 1;
+        } else {
+            self.current_idx -= 1;
+        }
+    }
+
+    pub fn next(&mut self) {
+        self.current_idx += 1;
+        if self.current_idx == self.chain.len() {
+            self.current_idx = 0;
+        }
+    }
+
+    pub fn current(&self) -> WeakHandle<dyn Control> {
+        self.chain[self.current_idx].clone()
     }
 }
 
